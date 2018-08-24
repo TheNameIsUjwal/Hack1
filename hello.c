dc1f1fa76c45d033f1436cf0356a60faf66894db
TheNameIsUjwal <33034938+TheNameIsUjwal@users.noreply.github.com>
2018-08-23 22:57:06 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+1 /**
+2 * Author: Ujwal Ghatamenini
+3 * Date: 2018/08/23
+4 *
+5 * A simple hello world program in C
+6 *
+7 */
+8 #include <stdlib.h>
+9 #include <stdio.h>
+10
+11 int main(int argc, char **argv) {
+12
+13 printf("Ujwal Ghatamenini. Computer Science is my Major!\n");
+14
+15 return 0;
+16 }
